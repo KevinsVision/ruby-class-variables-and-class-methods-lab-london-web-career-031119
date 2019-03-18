b8108f4312918a980ce81ec46de25cbc3d144a35
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-18 09:44:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,11 @@ class Song
   end
   
   def self.artist_count
-    
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      artist_count[artist] += 1
+    end
+    artist_count
   end
   
 end
